6753f88beec6352e5384aadf310f771f21ad77cd
mukil cb <mukil@grouptag.app>
2024-06-19 11:49:04 +1000

    initial commit

---
+++ compile.py
@@ -0,0 +1,14 @@
+from lexer import tokenize
+from parser import parse
+import os
+
+with open("test.rizz", "r") as f:
+    code = f.read()
+    tokens = list(tokenize(code))
+    result = "\n".join(parse(tokens))
+    filename = "./dist/index.js"
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    with open(filename, "w") as f:
+        f.write(result)
+        
+    print("Code written to", filename)
---
+++ lexer.py
@@ -0,0 +1,56 @@
+import re
+
+
+def tokenize(code):
+    token_specification = [
+        ("KEYWORD", r"(nocap|huh|finna|yap|sigma)"),  # Keywords
+        ("ASSIGN", r"="),  # Assignment operator
+        ("END", r";"),  # Statement terminator
+        ("ID", r"[A-Za-z_]\w*"),  # Identifiers
+        ("STRING", r"\".*?\""),  # String literals
+        ("LPAREN", r"\("),  # Left parenthesis
+        ("RPAREN", r"\)"),  # Right parenthesis
+        ("LBRACE", r"{"),  # Left curly brace
+        ("RBRACE", r"}"),  # Right curly brace
+        ("TEMPLATE_LITERAL", r"`[^`]*`"),  # Template literals (ensure proper handling)
+        ("INCREMENT", r"\+\+"),  # Increment operator
+        ("DECREMENT", r"--"),  # Decrement operator
+        ("OP", r"[+*\/-]"),  # Arithmetic operators
+        ("NUMBER", r"\d+(\.\d*)?"),  # Integer or decimal number
+        ("COMMA", r","),  # Comma
+        ("LT", r"<"),  # Less than
+        ("GT", r">"),  # Greater than
+        ("EQ", r"=="),  # Equal to
+        ("GE", r">="),  # Greater than or equal to
+        ("LE", r"<="),  # Less than or equal to
+        ("NE", r"!="),  # Not equal to
+        ("AND", r"&&"),  # Logical and
+        ("OR", r"\|\|"),  # Logical or
+        ("NEWLINE", r"\n"),  # Line endings
+        ("COMMENT", r"#.*"),  # Single-line comments
+        ("SKIP", r"[ \t]+"),  # Skip over spaces and tabs
+        ("MISMATCH", r"."),  # Any other character
+    ]
+
+    tok_regex = "|".join("(?P<%s>%s)" % pair for pair in token_specification)
+    get_token = re.compile(tok_regex).match
+    line_num = 1
+    mo = get_token(code)
+    pos = 0
+
+    while mo is not None:
+        kind = mo.lastgroup
+        value = mo.group(kind)
+        if kind == "NEWLINE":
+            line_num += 1
+        elif kind != "SKIP" and kind != "MISMATCH":
+            yield kind, value
+        elif kind == "COMMENT":
+            pass
+        elif kind == "MISMATCH":
+            raise RuntimeError(f"{value} unexpected on line {line_num}")
+        pos = mo.end()
+        mo = get_token(code, pos)
+
+    if pos != len(code):
+        raise RuntimeError(f"Unexpected character {code[pos]} on line {line_num}")
